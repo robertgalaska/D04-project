52933107eb71ec79ec92c622aec43aed6b59bd0b
CeriSlobbe <118452481+CeriSlobbe@users.noreply.github.com>
2022-11-17 17:30:59 +0100

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,5 +1,6 @@
+
 import numpy as np
-from constants import *
+import math
 
 wing_thickness = 0.05      #in meters
 x_co_engine = 0
@@ -10,28 +11,42 @@ thrust_cruise = 118.68      #In kN
 
 
 sweep_angle = 37.185*2*math.pi/360
-print(sweep_angle)
 
 def engineload_takeoff(thrust_cruise, sweep_angle, x,y,z):
 
-    engine_bending = np.ones((1,1)) # multiple with result from def
-    print(engine_bending)
+    engine_bending = []
+    engine_torque = []
+    
+    for i in range(1001):
+        
+        if i != 0:
+            
+            y_wing = (36.74/2)*(i/1000)
+            
+            if y_wing >= 6.43:
 
-    lst_engine_torque = []
-    for i in range(75):
-        y_wing = i/4
-        if y_wing >= 6.43:
+                
+                x_thrust = thrust_cruise*math.cos(sweep_angle)
+                y_thrust = thrust_cruise*math.sin(sweep_angle)
+                torque_engine = x_thrust*z
+                bending_moment_engine = y_thrust*z
+                engine_torque.append(torque_engine)
+                engine_bending.append(bending_moment_engine)
 
-            x_thrust = thrust_cruise*math.cos(sweep_angle)
-            y_thrust = thrust_cruise*math.sin(sweep_angle)
-            torque_engine = x_thrust*z
-            bending_moment_engine = y_thrust*z
+            else:
+                torque_engine = 0
+                bending_moment_engine = 0
+                engine_torque.append(torque_engine)
+                engine_bending.append(bending_moment_engine)
 
-        else:
-            torque_engine = 0
-            bending_moment_engine = 0
+    torque = np.array(engine_bending)
+    bending = np.array(engine_torque)
 
-engineload_takeoff(thrust_cruise, sweep_angle, x_co_engine, y_co_engine, z_co_engine)
+    return torque, bending
+
+x = engineload_takeoff(thrust_cruise, sweep_angle, x_co_engine, y_co_engine, z_co_engine)
+print(x)
+
 
 
 
