7d0a764e3e89fd1ca16bd1886e1f06bd321e47cf
barrom9 <j.barcinski@student.tudelft.nl>
2022-11-22 16:12:00 +0100

    polska do boju

--- deflecftion.py
+++ deflecftion.py
@@ -1,10 +1,14 @@
-# in order to calcuolate the deflection intergation has to be performed
+# in order to calculate the deflection intergation has to be performed
+import numpy as np
+import matplotlib.pyplot as plt
+
 M_x = []
 E= 68.9*10**9
 G=26*10**9
-from centroid import I_x, y
+from centroid import I_x, y, J
 import scipy as sp
 from scipy import integrate
+
 
 #Torque deflection:
 def T_integrand(T,G,J):
@@ -12,11 +16,15 @@ def T_integrand(T,G,J):
     return int
 
 from engine import x
-T = x[1]
+T = x[0]
 
+Theta = np.ones(1000)
 print(T)
-#T_int = T_integrand(T,G,J)
-#Theta = sp.integrate.trapezoid(y,T,0.0018,)
+for i in range(0,1000):
+    T_int = T_integrand(T[:i],G,J[:i])
+    Theta[i] = sp.integrate.trapezoid(y[:i],T_int)
+
+print(len(Theta))
 
 
 
