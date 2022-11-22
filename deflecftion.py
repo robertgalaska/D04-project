eb6e03f54abfc98e0bb4dabf2fa025a4a2c2badd
Guido-03 <G.vanderVeen@student.tudelft.nl>
2022-11-22 15:17:54 +0100

    start of deflection calculation

--- deflecftion.py
+++ deflecftion.py
@@ -1,4 +1,18 @@
-# in order to calcuolate the deflection intergation has to be performed
-M_x = []
+# in order to calculate the deflection intergation has to be performed
+import scipy as sp
+from scipy import integrate
+import numpy as np
+M_x = [2]
 E = 68.9*10**9
-I_
+from centroid import I_x
+from
+
+from centroid import halfspan
+# in order to integrate this we define a new function for scipy to intergrate
+
+def integrand_bending ():
+    int = M_x/(E*I_x)
+    return int
+#print(integrand_bending()[0])
+sp.integrate.trapezoid(integrand_bending(), , 0.0018)
+
