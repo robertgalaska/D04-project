0d8c45048c10f11877297afb6dfa97c18e7f4a29
Olivierbrunner04 <118452580+Olivierbrunner04@users.noreply.github.com>
2022-11-22 16:03:34 +0100

    Update aerodynamicLoads.py

--- aerodynamicLoads.py
+++ aerodynamicLoads.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 #import scipy as sp
 #from scipy import interpolate
 
-f = open("MainWing_a=0.00_v=10.00ms_1000steps.txt", "r")
+f = open("MainWing-a=0.00-v=10.00ms-1000steps.txt", "r")
 lines0 = f.readlines()
 f.close()
-g = open("MainWing_a=10.00_v=10.00ms_1000steps.txt", "r")
+g = open("MainWing-a=10.00-v=10.00ms-1000steps.txt", "r")
 lines10 = g.readlines()
 g.close()
 
@@ -45,10 +45,12 @@ def calculate_aeroloads(lines10, rho, v, q):
 
     locations0=[]
     liftcoefficients0=[]
+    dragcoefficients0=[]
     chords0=[]
     for i in range(100):
         locations0.append(array_values0[i,1])
         liftcoefficients0.append(array_values0[i,4])
+        dragcoefficients0.append(array_values0[i,6])
         chords0.append(array_values0[i,2])
 
     lift0 = []
@@ -58,10 +60,12 @@ def calculate_aeroloads(lines10, rho, v, q):
 
     locations10=[]
     liftcoefficients10=[]
+    dragcoefficients10=[]
     chords10=[]
     for i in range(100):
         locations10.append(array_values10[i,1])
         liftcoefficients10.append(array_values10[i,4])
+        dragcoefficients10.append(array_values0[i,6])
         chords10.append(array_values10[i,2])
 
     lift10 = []
@@ -71,12 +75,10 @@ def calculate_aeroloads(lines10, rho, v, q):
     moment0 = []
     moment10 = []
     for i in range(len(locations0)):
-        #length = round(len(locations0[i:-1])/3)
         length = float(locations0[i])+((halfspan-float(locations0[i])/3))
-        #moment0.append(float(locations0[length])*sum(lift0[i:-1]))
-        #moment10.append(float(locations0[length])*sum(lift10[i:-1]))
         moment0.append(length * sum(lift0[i:-1]))
         moment10.append(length * sum(lift10[i:-1]))
+
 
     fig, (ax1, ax2) = plt.subplots(2)
     fig.suptitle('Vertically stacked subplots')
@@ -90,8 +92,11 @@ def calculate_aeroloads(lines10, rho, v, q):
     aero_lift10 = np.array(lift10)
     aero_moment0 = np.array(moment0)
     aero_moment10 = np.array(moment10)
+    aero_induceddrag0 = np.array(dragcoefficients0)
+    aero_induceddrag10 = np.array(dragcoefficients10)
 
 
-    return aero_lift0, aero_lift10, aero_moment0, aero_moment10
+    return aero_lift0, aero_lift10, aero_moment0, aero_moment10, aero_induceddrag0, aero_induceddrag10 
 
-aero_lift0, aero_lift10, aero_moment0, aero_moment10 = calculate_aeroloads(lines10, rho, v, q)
+aero_lift0, aero_lift10, aero_moment0, aero_moment10, aero_induceddrag0, aero_induceddrag10 = calculate_aeroloads(lines10, rho, v, q)
+
