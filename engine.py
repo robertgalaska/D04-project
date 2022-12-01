97bc0f850b6af26116b1e5092382b2d58e30f1a5
Jozuaheule <118452285+Jozuaheule@users.noreply.github.com>
2022-12-01 16:53:46 +0100

    git add* git stash git pull

---
+++ engine.py
@@ -0,0 +1,63 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+wing_thickness = 0.05  # in meters
+x_co_engine = 0
+y_co_engine = 6.34
+z_co_engine = 1.2665
+thrust_takeoff = 120.64*10**3  # In N
+thrust_cruise = 118.68*10**3  # In N
+
+sweep_angle = 34.6 * 2 * math.pi / 360
+
+
+def engineload_takeoff(thrust_cruise, sweep_angle, x, y, z):
+    engine_bending = []
+    engine_torque = []
+    y_wing0 = []
+
+    for i in range(101):
+
+        if i != 0:
+
+            y_wing = (36.74 / 2) * (i / 100)
+            y_wing0.append(y_wing)
+
+            if y_wing <= 6.43:
+
+                x_thrust = thrust_cruise * math.cos(sweep_angle)
+                y_thrust = thrust_cruise * math.sin(sweep_angle)
+                torque_engine = x_thrust * z
+                bending_moment_engine = y_thrust * z
+                engine_torque.append(torque_engine)
+                engine_bending.append(bending_moment_engine)
+
+            else:
+                torque_engine = 0
+                bending_moment_engine = 0
+                engine_torque.append(torque_engine)
+                engine_bending.append(bending_moment_engine)
+
+    torque = np.array(engine_torque)
+    bending = np.array(engine_bending)
+    y_wing0 = np.array(y_wing0)
+
+    fig, axs = plt.subplots(2)
+    # first plot: torque
+    axs[0].plot(y_wing0, torque)
+    axs[0].set_title('Torque due to the engine thrust [Nm]')
+    axs[0].set_xlabel('Spanwise location [m]')
+    axs[0].set_ylabel('Torque [kNm]')
+    # second plot: bending moment
+    axs[1].plot(y_wing0, bending, 'tab:orange')
+    axs[1].set_title('Moment due to the engine thrust [Nm]')
+    axs[1].set_xlabel('Spanwise location [m]')
+    axs[1].set_ylabel('Moment [kNm]')
+    fig.tight_layout()
+    plt.show()
+
+    return torque, bending
+
+engine_torque, engine_bending = engineload_takeoff(thrust_cruise, sweep_angle, x_co_engine, y_co_engine, z_co_engine)
+
