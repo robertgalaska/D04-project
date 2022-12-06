369099fb4579e9a4cff00f5115a22b7284aef964
Jozuaheule <118452285+Jozuaheule@users.noreply.github.com>
2022-12-06 14:24:42 +0100

    update

--- main.py
+++ main.py
@@ -20,14 +20,14 @@ def combine_shearloads(aero_lift0, aero_lift10, inertial_shear):
     # Shear diagrams
     # first plot: shear AoA = 0
     axs[0].plot(y_wing0, total_shear0)
-    axs[0].set_title('Shear at angle of attack at 0 deg [Nm]')
+    axs[0].set_title('Shear at critical negative load factor')
     axs[0].set_xlabel('Spanwise location [m]')
-    axs[0].set_ylabel('Shear [kNm]')
+    axs[0].set_ylabel('Shear [N]')
     # second plot: shear AoA = 10
-    axs[1].plot(y_wing0, total_shear0, 'tab:orange')
-    axs[1].set_title('Shear at angle of attack at 10 deg [Nm]')
+    axs[1].plot(y_wing0, total_shear10, 'tab:orange')
+    axs[1].set_title('Shear at critical positive load factor')
     axs[1].set_xlabel('Spanwise location [m]')
-    axs[1].set_ylabel('Shear [Nm]')
+    axs[1].set_ylabel('Shear [N]')
     fig.tight_layout()
     plt.show()
 
@@ -45,14 +45,14 @@ def combine_bendingmoment(aero_moment0, aero_moment10, engine_bending, inertial_
     # Bending moments
     # first plot: bending moment AoA = 0
     axs[0].plot(y_wing0, total_bending_0)
-    axs[0].set_title('Moment at angle of attack at 0 deg [Nm]')
+    axs[0].set_title('Bending moment at critical negative load factor')
     axs[0].set_xlabel('Spanwise location [m]')
     axs[0].set_ylabel('Moment [kNm]')
     # second plot: bending moment AoA = 10
     axs[1].plot(y_wing0, total_bending_10, 'tab:orange')
-    axs[1].set_title('Moment at angle of attack at 10 deg [Nm]')
+    axs[1].set_title('Bending moment at critical positive load factor')
     axs[1].set_xlabel('Spanwise location [m]')
-    axs[1].set_ylabel('Moment [Nm]')
+    axs[1].set_ylabel('Moment [kNm]')
     fig.tight_layout()
     plt.show()
 
@@ -66,20 +66,20 @@ def combine_torque(engine_torque, aero_torque0, aero_torque10):
     # torque
     # first plot: torque AoA = 0
     axs[0].plot(y_wing0, total_torque0)
-    axs[0].set_title('Torque at angle of attack at 0 deg [Nm]')
+    axs[0].set_title('Torque at critical negative load factor')
     axs[0].set_xlabel('Spanwise location [m]')
-    axs[0].set_ylabel('Torque [Nm]')
+    axs[0].set_ylabel('Torque [kNm]')
     # second plot: torque AoA = 10
     axs[1].plot(y_wing0, total_torque10, 'tab:orange')
-    axs[1].set_title('Torque at angle of attack at 10 deg [Nm]')
+    axs[1].set_title('Torque at critical positive load factor')
     axs[1].set_xlabel('Spanwise location [m]')
-    axs[1].set_ylabel('Torque [Nm]')
+    axs[1].set_ylabel('Torque [kNm]')
     fig.tight_layout()
     plt.show()
 
     return total_torque0, total_torque10
 
-combine_shearloads(aero_lift0, aero_lift10, inertial_shear)
-combine_bendingmoment(aero_moment0, aero_moment10, engine_bending, inertial_moment)
-combine_torque(engine_torque, aero_torque0, aero_torque10)
+combine_shearloads(aero_shear_CLd_min, aero_shear_CLd_25, inertial_shear)
+combine_bendingmoment(aero_moment_CLd_min, aero_moment_CLd_25, engine_bending, inertial_moment)
+combine_torque(engine_torque, aero_torque_CLd_min, aero_torque_CLd_25)
 
