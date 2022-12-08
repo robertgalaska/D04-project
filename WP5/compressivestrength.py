0fa339128224b091df677b561886fa851ce3f8fa
robertgalaska <r.a.galaska@student.tudelft.nl>
2022-12-08 17:28:25 +0100

    NO comment

--- WP5/compressivestrength.py
+++ WP5/compressivestrength.py
@@ -1,15 +1,23 @@
 # Calculating the wing skin
-from centroid import I_x_s, I_x_c, area, I_x, Ix_frontspar, Ix_rearspar
+from centroid import corr_I_x_s, corr_I_x_c, area, corr_I_x, Ix_frontspar, Ix_rearspar, localchord, localt, a, b, I_s
 import numpy as np
+from scipy import pi
 from deflecftion import M_x, y
-a = M_x * y /(I_x_s + I_x_c)
-print(M_x * y /I_x_s)
+import matplotlib.pyplot as plt
 
-print( min (a ))
+areafrontspar = a * localt
+arearearspar = b * localt
+areastringer = 0.02 * 0.005 + 0.015 * 0.005
+c = 0.25
+e = 68.9 * 10 ** 9
+stressfrontspar = c * pi ** 2 * e * Ix_frontspar/ (y ** 2  *  areafrontspar)
+stressrearspar = c * pi ** 2 * e * Ix_rearspar/ (y ** 2  * arearearspar )
+stressstringer = c * pi ** 2 * e * I_s/ (y ** 2  * areastringer )
 
-print ( 0.794 * 3.14 ** 2 * 68.9 * 10 ** 9 * I_x/ ( y ** 2 * area))
 
-
+print(stressfrontspar)
+print(stressrearspar)
+print(stressstringer)
 
 
 
